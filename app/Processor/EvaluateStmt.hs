a9c03d59d637c053318a13c4640f2bb409c55e07
Gor Stepanyan <gor.stepanyan027@gmail.com>
2021-04-30 02:14:47 +0200

    Expression evaluation almost finished, types are fixed, compiles

--- app/Processor/EvaluateStmt.hs
+++ app/Processor/EvaluateStmt.hs
@@ -1,5 +1,6 @@
 module Processor.EvaluateStmt where
 
+import Control.Monad.Except
 import Grammar.Abs
 import Grammar.ErrM
 import Processor.Evaluate
@@ -26,38 +27,91 @@ import Processor.Evaluate
 --    | EAnd Expr Expr
 --    | EOr Expr Expr
 
-data Op = AddOp | MulOp deriving (Show, Read, Eq, Ord)
-
 -- TODO: Relational operations should be evaluated here
-evalOp :: Op -> Value -> Value -> Result Value
-evalOp Plus (VNum v1) (VNum v2) = return $ VNum $ v1 + v2
-evalOp Plus (VStr v1) (VStr v2) = return $ VStr $ v1 ++ v2
-evalOp Plus v1 v2 = throwError $ "Cannot add values of type " ++ show v1 ++ " and " ++ show v2
-evalOp Minus (VNum v1) (VNum v2) = return $ VNum $ v1 - v2
-evalOp Minus v1 v2 = throwError $ "Cannot subtract values of type " ++ show v1 ++ " and " ++ show v2
-evalOp Times (VNum v1) (VNum v2) = return $ VNum $ v1 * v2
-evalOp Times v1 v2 = throwError $ "Cannot multiply values of type " ++ show v1 ++ " and " ++ show v2
-evalOp Div (VNum v1) (VNum v2)
+evalAddOp :: AddOp -> Value -> Value -> Result Value
+evalAddOp Plus (VNum v1) (VNum v2) = return $ VNum $ v1 + v2
+evalAddOp Plus (VStr v1) (VStr v2) = return $ VStr $ v1 ++ v2
+evalAddOp Plus v1 v2 = throwError $ "Cannot add values of type " ++ show v1 ++ " and " ++ show v2
+evalAddOp Minus (VNum v1) (VNum v2) = return $ VNum $ v1 - v2
+evalAddOp Minus v1 v2 = throwError $ "Cannot subtract values of type " ++ show v1 ++ " and " ++ show v2
+
+evalMulOp :: MulOp -> Value -> Value -> Result Value
+evalMulOp Times (VNum v1) (VNum v2) = return $ VNum $ v1 * v2
+evalMulOp Times v1 v2 = throwError $ "Cannot multiply values of type " ++ show v1 ++ " and " ++ show v2
+evalMulOp Div (VNum v1) (VNum v2)
   | v2 == 0 = throwError "Cannot divide by 0"
   | otherwise = return $ VNum $ div v1 v2
-evalOp Div v1 v2 = throwError $ "Cannot divide values of type " ++ show v1 ++ " and " ++ show v2
-evalOp Mod (VNum v1) (VNum v2)
+evalMulOp Div v1 v2 = throwError $ "Cannot divide values of type " ++ show v1 ++ " and " ++ show v2
+evalMulOp Mod (VNum v1) (VNum v2)
   | v2 == 0 = throwError "Cannot find remainder for division by 0"
   | otherwise = return $ VNum $ mod v1 v2
+
+isLess :: Value -> Value -> Result Value
+isLess (VBool v1) (VBool v2) = return . VBool $ (v1 < v2)
+isLess (VNum v1) (VNum v2) = return . VBool $ (v1 < v2)
+isLess (VStr v1) (VStr v2) = return . VBool $ (v1 < v2)
+isLess v1 v2 = throwError $ "Cannot comapre values of type " ++ show v1 ++ " and " ++ show v2
+
+isEqual :: Value -> Value -> Result Value
+isEqual v1 v2 = do
+  (VBool res1) <- isLess v1 v2
+  (VBool res2) <- isLess v2 v1
+  return . VBool $ not res1 && not res2
+
+-- LTH | LE | GTH | GE | EQU | NE
+evalRelOp :: RelOp -> Value -> Value -> Result Value
+evalRelOp LTH v1 v2 = isLess v1 v2
+evalRelOp LE v1 v2 = do
+  (VBool res1) <- isLess v1 v2
+  (VBool res2) <- isEqual v1 v2
+  return . VBool $ res1 || res2
+evalRelOp GTH v1 v2 = isLess v2 v1
+evalRelOp GE v1 v2 = do
+  (VBool res1) <- isLess v2 v1
+  (VBool res2) <- isEqual v1 v2
+  return . VBool $ res1 || res2
+evalRelOp EQU v1 v2 = isEqual v1 v2
+evalRelOp NE v1 v2 = do
+  (VBool res) <- isEqual v1 v2
+  return . VBool $ not res
 
 evalExp :: Expr -> Result Value
 evalExp (EVar i) = getValueByIdent i
 evalExp (ELitInt i) = return $ VNum i
 evalExp ELitTrue = return $ VBool True
 evalExp ELitFalse = return $ VBool False
-evalExp (EApp i es) = undefined
+evalExp (EApp i es) = undefined -- TODO: Assign
 evalExp (EString s) = return $ VStr s
-evalExp (ELambda args t b) = undefined
+evalExp (ELambda args t b) = undefined -- TODO design
 evalExp (Neg e) = do
-  val <- VNum (evalExp e)
-  return $ VNum (- val)
+  val <- evalExp e
+  case val of
+    (VNum v) -> return $ VNum (- v)
+    _ -> throwError $ "Cannot change sign of a value of type " ++ show val
 evalExp (Not e) = do
-  val <- VBool (evalExp e)
-  return $ VBool $ not val
-evalExp (EAdd e1 op e2) = liftM2 (evalOp op) (evalExp e1) (evalExp e2)
-evalExp (EMul e1 op e2) = liftM2 (evalOp op) (evalExp e1) (evalExp e2)
+  val <- evalExp e
+  case val of
+    (VBool v) -> return $ VBool (not v)
+    _ -> throwError $ "Cannot negate a value of type " ++ show val
+evalExp (EAdd e1 op e2) = do
+  v1 <- evalExp e1
+  v2 <- evalExp e2
+  evalAddOp op v1 v2
+evalExp (EMul e1 op e2) = do
+  v1 <- evalExp e1
+  v2 <- evalExp e2
+  evalMulOp op v1 v2
+evalExp (ERel e1 op e2) = do
+  v1 <- evalExp e1
+  v2 <- evalExp e2
+  evalRelOp op v1 v2
+evalExp (EAnd e1 e2) = do
+  (VBool val1) <- evalExp e1
+  if not val1
+    then return . VBool $ False
+    else evalExp e2
+evalExp (EOr e1 e2) = do
+  (VBool val1) <- evalExp e1
+  if val1
+    then return . VBool $ True
+    else evalExp e2
